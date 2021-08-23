ff6e1c327036bad831b3dfafdefae4f0524ba250
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-08-23 18:46:52 -0300

    add birthday

--- primeiro-programa-java/src/curso/pessoa.java
+++ primeiro-programa-java/src/curso/pessoa.java
@@ -1,5 +1,29 @@
 package curso;
+import java.util.Date;
 
 public class pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+	
+	private Date dataNascimento;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 }
