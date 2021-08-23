6412cd65e40d47d7bb0058ec446edc47b163de72
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-08-23 18:46:52 -0300

    add getter and setter for class pessoa

--- primeiro-programa-java/src/curso/pessoa.java
+++ primeiro-programa-java/src/curso/pessoa.java
@@ -1,5 +1,26 @@
 package curso;
 
 public class pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 }
