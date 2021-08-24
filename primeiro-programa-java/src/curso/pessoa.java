2c53d5ca891401898bdf1b85a937f27ba17cc12a
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-08-24 18:20:07 -0300

    add birthday getter and setter

--- primeiro-programa-java/src/curso/pessoa.java
+++ primeiro-programa-java/src/curso/pessoa.java
@@ -13,6 +13,14 @@ public class pessoa {
 		return nome;
 	}
 
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
