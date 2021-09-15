bd7b29e9fa552ad737d82dc8c3f0b15d74bc755d
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-09-14 23:08:03 -0300

    add student's getters and setters and score function

--- primeiro-programa-java/src/cursojava/classes/Student.java
+++ primeiro-programa-java/src/cursojava/classes/Student.java
@@ -1,16 +1,21 @@
 package cursojava.classes;
 
 public class Student {
-	String name;
-	int age;
-	String birthday;
-	String id;
-	String cpfNumber;
-	String motherName;
-	String fatherName;
-	String registrationDate;
-	String schoolName;
-	String educationLevel;
+	private String name;
+	private int age;
+	private String birthday;
+	private String id;
+	private String cpfNumber;
+	private String motherName;
+	private String fatherName;
+	private String registrationDate;
+	private String schoolName;
+	private String educationLevel;
+	
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
 	
 	public Student() {
 		
@@ -18,5 +23,121 @@ public class Student {
 	
 	public Student (String nameDefault) {
 		name = nameDefault;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCpfNumber() {
+		return cpfNumber;
+	}
+
+	public void setCpfNumber(String cpfNumber) {
+		this.cpfNumber = cpfNumber;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(String registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public String getEducationLevel() {
+		return educationLevel;
+	}
+
+	public void setEducationLevel(String educationLevel) {
+		this.educationLevel = educationLevel;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	
+	public double getStudentScore() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
 	}
 }
--- primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -10,12 +10,21 @@ public class primeiraClasseJava {
 	public static void main(String[] args) {
 		
 		Student aluno1 = new Student();
+		aluno1.setNota1(7.5);
+		aluno1.setNota2(6);
+		aluno1.setNota3(8);
+		aluno1.setNota4(9);
 		
 		Student aluno2 = new Student();
+		aluno2.setNota1(2);
+		aluno2.setNota2(4);
+		aluno2.setNota3(3);
+		aluno2.setNota4(3.5);
 		
-		Student aluno3 = new Student("Bruno");
+		Student aluno3 = new Student();
 		
-		System.out.println(aluno3);
+		System.out.println("Média do aluno1: " + aluno1.getStudentScore());
+		System.out.println("Média do aluno2: " + aluno2.getStudentScore());
 		
 	}
 }
