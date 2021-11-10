f512957c9d7dbd5a2f0768c910c84d1c42396fca
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-11-09 23:05:35 -0300

    comentando  setNota

--- primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/primeiraClasseJava.java
@@ -68,7 +68,7 @@ public class primeiraClasseJava {
 					
 					Course course = new Course();
 					course.setCourse1(courseName);
-					course.setNota(Double.valueOf(courseScore));
+					//course.setNota(Double.valueOf(courseScore));
 					
 					aluno.getCourses().add(course);
 				}
